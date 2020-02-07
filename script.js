177458269a05c1d817b580e228ca82355ea12332
ogbeche77 <ogbeche77@yahoo.com>
2020-02-07 16:09:20 +0100

    width

--- script.js
+++ script.js
@@ -18,62 +18,58 @@ async function getData() {
     table.forEach(row =>{
         const columns =row.split(",");
        const year =columns[0];
-       /*  xlabels.push(year); */ //chart ref can be added here
+        xyears.push(year);  //chart ref can be added here
        const unEmploy =columns[9];
-         /* avgMonths.push(tempA); */  //chart ref can be added here
+         empPercent.push(unEmploy);   //chart ref can be added here
         
         console.log(year,unEmploy);
     });
 }
-getData();
 
 
 
-/*
 
 // Presenting the result of the ocean mean temperature in a chart
 
 // To present as chart we create a new variable and set to blank array
-const xlabels =[];  //important its made a global variable
-const avgMonths = [];  //important its made a global variable
+const xyears =[];  //important its made a global variable
+const empPercent = [];  //important its made a global variable
               //important its made a global variable
 
-chartIt();  //calling function (position matter)
+ empChart();  //calling function (position matter)
 
-async function chartIt(){
-    await getData();  //after chartIt is called, it waits for getData() before executing
+async function empChart(){
+    await getData();  //after empChart is called, it waits for getData() before executing
 const ctx = document.getElementById('chart').getContext('2d');
 
-
-
-
 const myChart = new Chart(ctx, {
-    type: 'line',
+    type: 'pie',
     data: {
-        labels: xlabels,  //const newLabel is referenced here
+        labels: xyears,  //const newLabel is referenced here
         datasets: [{
-            label: 'Ocean Avg temperature (Global in C°)',
-            data: avgMonths,
+            label: 'Unemployment (In %)',
+            data: empPercent,
             backgroundColor: 'rgba(235, 186, 52 0.2)',
-            borderColor: 'rgba(235, 186, 52)',
-            borderWidth: 1,
+            borderColor: 'rgb(45, 233, 247)',
+            borderWidth: 2,
             fill: false,
            }],    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    // This call backfunction can be used to edit the y-axis
-                    callback: function(value, index, values) {
-                        return value + "°C" ;
+           options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        // This call backfunction can be used to edit the y-axis
+                        callback: function(value, index, values) {
+                            return value + "%" ;
+                        }
                     }
-                }
+    
+    
+                }]
+         }
+         
+        }
 
-
-            }]
-     }
-     
-    }
     });
 }
- */
+ 
